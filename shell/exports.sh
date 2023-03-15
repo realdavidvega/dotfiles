c2152ea515e3be5abb5d55427939ef159924d1d3
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2023-03-15 11:58:52 +0100

    fix: change load order and add new git config

--- shell/exports.sh
+++ shell/exports.sh
@@ -49,6 +49,74 @@ path=(
 
 export path
 
+# -----------------------------------------------------------------
+#
+# ░█████╗░░██████╗░░░░░░░██████╗███████╗████████╗██╗░░░██╗██████╗░
+# ██╔══██╗██╔════╝░░░░░░██╔════╝██╔════╝╚══██╔══╝██║░░░██║██╔══██╗
+# ██║░░██║╚█████╗░█████╗╚█████╗░█████╗░░░░░██║░░░██║░░░██║██████╔╝
+# ██║░░██║░╚═══██╗╚════╝░╚═══██╗██╔══╝░░░░░██║░░░██║░░░██║██╔═══╝░
+# ╚█████╔╝██████╔╝░░░░░░██████╔╝███████╗░░░██║░░░╚██████╔╝██║░░░░░
+# ░╚════╝░╚═════╝░░░░░░░╚═════╝░╚══════╝░░░╚═╝░░░░╚═════╝░╚═╝░░░░░
+#
+# -----------------------------------------------------------------
+
+# Linux
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
+  # Paths
+  OS_DRIVE="/mnt"
+  OS_WORKSPACE="${OS_DRIVE}/d/Workspace" 
+
+  # Apps
+  alias open="explorer.exe"
+  alias sublime="${OS_DRIVE}/c/Program\ Files/Sublime\ Text/sublime_text.exe"
+  alias vscode="${OS_DRIVE}/c/Users/david/AppData/Local/Programs/Microsoft\ VS\ Code/Code.exe" 
+
+# MacOS
+elif [[ "$OSTYPE" =~ ^darwin ]]; then
+
+  # NVM env
+  export NVM_DIR="$HOME/.nvm"
+  source $(brew --prefix nvm)/nvm.sh
+
+  # Node path
+  export NODE_PATH=$NODE_PATH:`npm root -g`
+
+  # Ruby env
+  eval "$(rbenv init - zsh)"
+
+  # TheFuck
+  eval $(thefuck --alias)
+
+  # Paths
+  OS_DRIVE="/Volumes/Macintosh\ HD" 
+  OS_WORKSPACE="~/Workspace" 
+
+  # Apps
+  alias sublime="open ${OS_DRIVE}/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
+  alias vscode="open ${OS_DRIVE}/Applications/Visual\ Studio\ Code.app"  
+
+  # Terminal
+  alias ls="exa"
+  alias l="exa -l"
+  alias ll="exa -la"
+
+  # Daily wallpaper (execute once, node needed)
+  # npx --yes bing-wallpaper-daily-mac-multimonitor@latest enable-auto-update
+
+# Cygwin
+elif [[ "$OSTYPE" == "cygwin"* ]]; then
+
+  # Paths
+  OS_DRIVE="/cygdrive" 
+  OS_WORKSPACE="${OS_DRIVE}/d/Workspace" 
+
+  # Apps
+  alias su="${OS_DRIVE}/c/Program\ Files/Sublime\ Text/sublime_text.exe"
+  alias vsc="${OS_DRIVE}/c/Users/david/AppData/Local/Programs/Microsoft\ VS\ Code/Code.exe"  
+
+fi
+
 # ---------------------------
 #        Custom Paths
 # ---------------------------
