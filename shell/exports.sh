3fc8229b7a80bab1749d81acab192d3c3f5351a5
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2023-03-25 18:07:48 +0100

    fix: better functioning on WSL2

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,54 +1,3 @@
-# ------------------------------------------------------------------------------
-# Codely theme config
-# ------------------------------------------------------------------------------
-export CODELY_THEME_MINIMAL=false
-export CODELY_THEME_MODE="dark"
-export CODELY_THEME_PROMPT_IN_NEW_LINE=false
-export CODELY_THEME_PWD_MODE="short" # full, short, home_relative
-
-# ------------------------------------------------------------------------------
-# Languages
-# ------------------------------------------------------------------------------
-export JAVA_HOME='/Users/david/Library/Java/JavaVirtualMachines/corretto-17.0.6/Contents/Home'
-export GEM_HOME="$HOME/.gem"
-export GOPATH="$HOME/.go"
-
-# ------------------------------------------------------------------------------
-# Apps
-# ------------------------------------------------------------------------------
-if [ "$CODELY_THEME_MODE" = "dark" ]; then
-	fzf_colors="pointer:#ebdbb2,bg+:#3c3836,fg:#ebdbb2,fg+:#fbf1c7,hl:#8ec07c,info:#928374,header:#fb4934"
-else
-	fzf_colors="pointer:#db0f35,bg+:#d6d6d6,fg:#808080,fg+:#363636,hl:#8ec07c,info:#928374,header:#fffee3"
-fi
-
-export FZF_DEFAULT_OPTS="--color=$fzf_colors --reverse"
-
-# ------------------------------------------------------------------------------
-# Path - The higher it is, the more priority it has
-# ------------------------------------------------------------------------------
-path=(
-	"$HOME/bin"
-	"$DOTLY_PATH/bin"
-	"$DOTFILES_PATH/bin"
-	"$JAVA_HOME/bin"
-	"$GEM_HOME/bin"
-	"$GOPATH/bin"
-	"$HOME/.cargo/bin"
-	"/usr/local/opt/ruby/bin"
-	"/usr/local/opt/python/libexec/bin"
-	"/opt/homebrew/bin"
-	"/usr/local/bin"
-	"/usr/local/sbin"
-	"/bin"
-	"/usr/bin"
-	"/usr/sbin"
-	"/sbin"
-	"$path"
-)
-
-export path
-
 # -----------------------------------------------------------------
 #
 # ░█████╗░░██████╗░░░░░░░██████╗███████╗████████╗██╗░░░██╗██████╗░
@@ -63,14 +12,19 @@ export path
 # Linux
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
+  export HOME="/home/david"
+
   # Paths
   OS_DRIVE="/mnt"
   OS_WORKSPACE="${OS_DRIVE}/d/Workspace" 
 
   # Apps
-  alias open="explorer.exe"
+  alias open="${OS_DRIVE}/c/Windows/SysWOW64/explorer.exe"
   alias sublime="${OS_DRIVE}/c/Program\ Files/Sublime\ Text/sublime_text.exe"
   alias vscode="${OS_DRIVE}/c/Users/david/AppData/Local/Programs/Microsoft\ VS\ Code/Code.exe" 
+
+  # Workaround for slow git on WSL2
+  alias git="/mnt/c/Program\ Files/Git/bin/git.exe"
 
 # MacOS
 elif [[ "$OSTYPE" =~ ^darwin ]]; then
@@ -117,6 +71,63 @@ elif [[ "$OSTYPE" == "cygwin"* ]]; then
 
 fi
 
+# ------------------------------------------------------------------------------
+# Dotly Paths
+# ---------------------------------------------------------------
+export DOTFILES_PATH="$HOME/.dotfiles"
+export DOTLY_PATH="$DOTFILES_PATH/modules/dotly" 
+
+# ------------------------------------------------------------------------------
+# Codely theme config
+# ------------------------------------------------------------------------------
+export CODELY_THEME_MINIMAL=false
+export CODELY_THEME_MODE="dark"
+export CODELY_THEME_PROMPT_IN_NEW_LINE=false
+export CODELY_THEME_PWD_MODE="short" # full, short, home_relative
+
+# ------------------------------------------------------------------------------
+# Languages
+# ------------------------------------------------------------------------------
+export JAVA_HOME='$HOME/Library/Java/JavaVirtualMachines/corretto-17.0.6/Contents/Home'
+export GEM_HOME="$HOME/.gem"
+export GOPATH="$HOME/.go"
+
+# ------------------------------------------------------------------------------
+# Apps
+# ------------------------------------------------------------------------------
+if [ "$CODELY_THEME_MODE" = "dark" ]; then
+	fzf_colors="pointer:#ebdbb2,bg+:#3c3836,fg:#ebdbb2,fg+:#fbf1c7,hl:#8ec07c,info:#928374,header:#fb4934"
+else
+	fzf_colors="pointer:#db0f35,bg+:#d6d6d6,fg:#808080,fg+:#363636,hl:#8ec07c,info:#928374,header:#fffee3"
+fi
+
+export FZF_DEFAULT_OPTS="--color=$fzf_colors --reverse"
+
+# ------------------------------------------------------------------------------
+# Path - The higher it is, the more priority it has
+# ------------------------------------------------------------------------------
+path=(
+	"$HOME/bin"
+	"$DOTLY_PATH/bin"
+	"$DOTFILES_PATH/bin"
+	"$JAVA_HOME/bin"
+	"$GEM_HOME/bin"
+	"$GOPATH/bin"
+	"$HOME/.cargo/bin"
+	"/usr/local/opt/ruby/bin"
+	"/usr/local/opt/python/libexec/bin"
+	"/opt/homebrew/bin"
+	"/usr/local/bin"
+	"/usr/local/sbin"
+	"/bin"
+	"/usr/bin"
+	"/usr/sbin"
+	"/sbin"
+	"$path"
+)
+
+export path
+
 # ---------------------------
 #        Custom Paths
 # ---------------------------
@@ -151,5 +162,5 @@ export RUST="${GITHUB}/rust"
 export WEB="${GITHUB}/web"
 
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-export PATH="/Users/david/.rd/bin:$PATH"
+export PATH="$HOME/.rd/bin:$PATH"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
