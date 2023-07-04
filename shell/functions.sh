7c2f3f79e7834606be24bde25d0f046af11be9be
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2023-07-04 17:04:24 +0200

    fix: make git functions part of gitalias instead

--- shell/functions.sh
+++ shell/functions.sh
@@ -23,21 +23,3 @@ tere() {
   local result=$(command tere "$@")
   [ -n "$result" ] && cd -- "$result"
 }
-
-# Open the Pull Request URL for your current directory's branch (base branch defaults to main)
-function openpr() {
-  github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's@com:@com/@' -e 's%\.git$%%' | awk '/github/'`;
-  branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
-  pr_url=$github_url"/compare/main..."$branch_name
-  open $pr_url;
-}
- 
-# Run git push and then immediately open the Pull Request URL
-function gpr() {
-  git push origin HEAD
- 
-  if [ $? -eq 0 ]; then
-    openpr
-  else
-    echo 'failed to push commits and open a pull request.';
-  fi
