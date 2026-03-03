8c133a07319cd641200d232317c5267847f66d7c
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2026-03-03 06:30:46 +0100

    feat: replace z with zoxide

--- shell/functions.sh
+++ shell/functions.sh
@@ -7,11 +7,6 @@ function cdd() {
 	cd "$(ls -d -- */ | fzf)" || echo "Invalid directory"
 }
 
-function j() {
-	fname=$(declare -f -F _z)
-	[ -n "$fname" ] || source "$DOTLY_PATH/modules/z/z.sh"
-	_z "$1"
-}
 
 # Get recent dirs
 function recent_dirs() {
